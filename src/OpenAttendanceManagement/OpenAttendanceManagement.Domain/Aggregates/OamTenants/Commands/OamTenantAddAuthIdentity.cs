07a494a65420d900d850e20760b4b822ec5175fc
Tomohisa Takaoka <ttakaoka@jtechs.com>
2024-08-09 12:51:18 -0700

    Update OamTenantAddAuthIdentity.cs

--- src/OpenAttendanceManagement/OpenAttendanceManagement.Domain/Aggregates/OamTenants/Commands/OamTenantAddAuthIdentity.cs
+++ src/OpenAttendanceManagement/OpenAttendanceManagement.Domain/Aggregates/OamTenants/Commands/OamTenantAddAuthIdentity.cs
@@ -24,20 +24,3 @@ public record OamTenantAddAuthIdentity(
             .Conveyor(_ => context.AppendEvent(new OamTenantAuthIdentityEmailAdded(command.Email)));
     public string TenantId => TenantCode.Value;
 }
-public record OamTenantRemoveAuthIdentity(
-    OamTenantId OamTenantId,
-    AuthIdentityEmail Email,
-    TenantCode TenantCode) : ITenantCommandWithHandler<OamTenant, OamTenantRemoveAuthIdentity>
-{
-    public Guid GetAggregateId() => OamTenantId.Value;
-    public static ResultBox<UnitValue> HandleCommand(
-        OamTenantRemoveAuthIdentity command,
-        ICommandContext<OamTenant> context) =>
-        ResultBox.FromValue(context.GetState())
-            .Verify(
-                state => state.Payload.Admins.Any(x => x.Value == command.Email.Value)
-                    ? ExceptionOrNone.None : new TenantAdminNotFound("指定された管理者が見つかりません。"))
-            .Conveyor(
-                _ => context.AppendEvent(new OamTenantAuthIdentityEmailRemoved(command.Email)));
-    public string TenantId => TenantCode.Value;
-}
