8b66f512bbf53fa211a53d49dde15748437e42cd
Tomohisa Takaoka <ttakaoka@jtechs.com>
2024-09-25 23:41:26 -0700

    Blazor Web.

---
+++ src/OpenAttendanceManagement/OpenAttendanceManagement.Web.Keycloak/LoginLogoutEndpointRouteBuilderExtensions.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+namespace OpenAttendanceManagement.Web.Keycloak;
+
+public static class LoginLogoutEndpointRouteBuilderExtensions
+{
+
+    public static IEndpointConventionBuilder MapLoginAndLogout(this IEndpointRouteBuilder endpoints)
+    {
+        var group = endpoints.MapGroup("authentication");
+
+        group
+            .MapGet("/login", () => TypedResults.Challenge(new AuthenticationProperties { RedirectUri = "/" }))
+            .AllowAnonymous();
+
+        group.MapPost(
+            "/logout",
+            () => TypedResults.SignOut(
+                new AuthenticationProperties { RedirectUri = "/" },
+                [CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme]));
+
+        return group;
+    }
+}
