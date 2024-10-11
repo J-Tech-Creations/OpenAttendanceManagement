00dd634bdfa8fac2ee81db20aef65ad7b1e0cda6
Tomohisa Takaoka <ttakaoka@jtechs.com>
2024-10-11 15:43:38 -0700

    Refactoring

--- src/OpenAttendanceManagement/OpenAttendanceManagement.Common/UseCases/ISekibanUsecaseAsync.cs
+++ src/OpenAttendanceManagement/OpenAttendanceManagement.Common/UseCases/ISekibanUsecaseAsync.cs
@@ -1,7 +1,7 @@
 using ResultBoxes;
 namespace OpenAttendanceManagement.Common.UseCases;
 
-public interface ISekibanUsecaseAsync<TIn, TOut> where TIn : class, ISekibanUsecaseAsync<TIn, TOut>, IEquatable<TIn>
+public interface ISekibanUsecaseAsync<in TIn, TOut> where TIn : class, ISekibanUsecaseAsync<TIn, TOut>, IEquatable<TIn>
     where TOut : notnull
 {
     public static abstract Task<ResultBox<TOut>> ExecuteAsync(TIn input, ISekibanUsecaseContext context);
